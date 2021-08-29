e6ddaaf4badefdb33ee93f48836dda8c1f9acc9f
Nathamuni <nathamuninathamuni9960@gmail.com>
2021-08-29 18:57:23 +0530

    All [OOPs]concepts of Java

--- src/com/lao/Constructor/ParentClass.java
+++
@@ -1,8 +0,0 @@
-package com.lao.Constructor;
-
-public class ParentClass {
-    public ParentClass() //We can have access modifier (PUBLIC)
-    {
-        System.out.println("parent class constructor");
-    }
-}
