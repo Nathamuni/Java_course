d5597449241878cc34078291dea80484db8eacc2
Nathamuni <87070459+Nathamuni@users.noreply.github.com>
2021-08-30 14:18:21 +0530

    Guess the output..!

--- src/TestMyself/prob11.java
+++ src/TestMyself/prob11.java
@@ -3,7 +3,8 @@ package TestMyself;
 public class prob11 {
     public static void main(String[] args) {
         int a = 027;
+        int b = 028;
         System.out.println(a);
-
+        System.out.println(b);
     }
 }
