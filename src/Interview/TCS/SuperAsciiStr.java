ce9cf203f7341b6d47f5e18271827e3e4b7a7079
Nathamuni <nathamuninathamuni9960@gmail.com>
2022-11-23 14:29:30 +0530

    No Comments Only Commits

--- src/Interview/TCS/SuperAsciiStr.java
+++ src/Interview/TCS/SuperAsciiStr.java
@@ -1,5 +1,6 @@
 package Interview.TCS;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class SuperAsciiStr {
@@ -10,15 +11,31 @@ public class SuperAsciiStr {
         for(int i = 97;i<=122;i++){
             hs.put((char)i,i-96);
         }
-        System.out.println(SuAss("accbc"));
+        System.out.println(SuAss("ddeabbddccc"));
     }
 
-    private static String SuAss(String s) {
-        for (int i=0;i<s.length();i++) {
-            char c = s.charAt(i);
-            String a =s.replaceAll(Character.toString(c),"");
-            if(s.length()-a.length() != hs.get(c)) return "No";
+//    private static String SuAss(String s) {
+//        for (int i=0;i<s.length();i++) {
+//            char c = s.charAt(i);
+//            String a =s.replaceAll(Character.toString(c),"");
+//            if(s.length()-a.length() != hs.get(c)) return "No";
+//        }
+//        return "Yes";
+//    }
+private static String SuAss(String s) {
+        //better
+        char[] ch = s.toCharArray();
+          Arrays.sort(ch);
+
+    for (int i=0;i<ch.length;i++) {
+        int count =1;
+        while(i+1<ch.length && ch[i] == ch[i+1]) {
+            count++;
+            i++;
         }
-        return "Yes";
+        if(ch[i]-96 != count)
+            return "No";
     }
+    return "Yes";
+}
 }
