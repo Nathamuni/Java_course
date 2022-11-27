6bb1958e1ad250900edebec29c8eec38f200df99
Nathamuni <nathamuninathamuni9960@gmail.com>
2022-11-27 18:27:24 +0530

    Commit

--- src/Interview/TCS/SecondMostFreqOccrStr.java
+++ src/Interview/TCS/SecondMostFreqOccrStr.java
@@ -1,5 +1,8 @@
 package Interview.TCS;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class SecondMostFreqOccrStr {
     public static void main(String[] args) {
         String []s = {"aaa","bbb","ccc","bbb"};
@@ -7,21 +10,20 @@ public class SecondMostFreqOccrStr {
     }
 
     private static String secFreqEle(String[] s) {
+        HashMap<Integer,String> hs = new HashMap<>();
         int max=0,SecondMax=0;
         String out="";
 
         for(int i=0; i< s.length;i++) {
+
             int count=0;
+
             for(int j=i; j< s.length;j++) {
                 if(s[i].equals(s[j]))
                     count++;
-            }
-            if((count < max && count > SecondMax)){
-                SecondMax = max;
-                out = s[i];
-            }
-            max = Math.max(count ,max);
 
+            }
+            hs.put(count, s[i]);
 
         }
         return  out;
