ac33369371583de5565db4594a5c86a1d1097d2d
Nathamuni <87070459+Nathamuni@users.noreply.github.com>
2023-02-14 22:11:45 +0530

    Create SubSeq.java

---
+++ src/Interview/randomPreparation/SubSeq.java
@@ -0,0 +1,17 @@
+package Interview.randomPreparation;
+
+public class SubSeq {
+    public static void main(String[] args) {
+        sub("","abc ");
+    }
+    public static void sub(String processed,String Unprocessed){
+        if(Unprocessed.isEmpty())
+        {
+            System.out.println(processed);
+            return;
+        }
+        char c = Unprocessed.charAt(0);
+        sub(processed+c,Unprocessed.substring(1));
+        sub(processed,Unprocessed.substring(1));
+    }
+}
