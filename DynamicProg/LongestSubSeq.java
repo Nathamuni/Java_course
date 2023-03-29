6441c0bc4f332d0dd9a585980f39ceade88d05fc
Nathamuni <87070459+Nathamuni@users.noreply.github.com>
2023-03-29 16:06:28 +0530

    Update LongestSubSeq.java

--- DynamicProg/LongestSubSeq.java
+++ DynamicProg/LongestSubSeq.java
@@ -3,7 +3,7 @@ package Interview.DynamicProg;
 public class LongestSubSeq {
    static  String s1 ="aertbaaaaaa6666c",s2="bcdewraaat";
     public static void main(String[] args) {
-        System.out.println(f1(s1.length()-1,s2.length()-1));
+        System.out.println(f(s1.length()-1,s2.length()-1));
     }
 
     private static int f1  (int i1, int i2) {
@@ -15,13 +15,13 @@ public class LongestSubSeq {
         return 0 +Math.max(f(i1-1,i2), f(i1,i2-1));
     }
     private static int f(int i1, int i2) {
-        int dp[][] = new int[i1+1][i2+1];
+        int dp[][] = new int[s1.length()+1][s2.length()+1];
         if(i1<0 || i2<0)
             return 0;
         if(dp[i1][i2] != 0 )return dp[i1][i2];
         if(s1.charAt(i1)==s2.charAt(i2))
-            return dp[i1][i2]= 1 + f(i1-1,i2-1);
+            return dp[i1+1][i2+1]= 1 + f(i1-1,i2-1);
 
-        return  dp[i1][i2]=Math.max(f(i1-1,i2), f(i1,i2-1));
+        return  dp[i1+1][i2+1]=Math.max(f(i1-1,i2), f(i1,i2-1));
     }
 }
